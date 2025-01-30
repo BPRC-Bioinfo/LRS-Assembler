e21d5e213a908a1a0f5714821bc99d7e30bc5c83
Giang <giangln@gmail.com>
2025-01-30 21:57:55 +0000

    retain multiple copies

--- annotation/analyse_paf_genomic.py
+++ annotation/analyse_paf_genomic.py
@@ -70,44 +70,85 @@ def group_by_ref_name(file_path):
 
             # Calculate alignment length if BLAST results present
             aligned_length = sum(int(parts[6]) if len((parts := last.strip("\n").split(" "))) > 10 else 0 for _, _, _, _, _, last in subgroup) / length *100
+#            print (ref_name)
 #            print (aligned_length)
 
             # Skip processing the whole subgroup if aligned_length exceeds 100
             if aligned_length > 100:
+                fragment_groups = []
+                previous_end_coord = None
                 # Loop through each line in the subgroup
                 for line_idx, (line_ref_start_coord, line_ref_end_coord, line_chr_start_coord, line_chr_end_coord, _, line_last) in enumerate(subgroup):
                     line_parts = line_last.strip("\n").split(" ")
+ #                   print (line_parts)
+                    start_coord = int(line_parts[1])
+ #                   print(start_coord)
+                    if previous_end_coord is not None and start_coord <= previous_end_coord:
+ #                       print ("part1")
+                        fragment_groups.append((line_ref_start_coord, line_ref_end_coord, line_chr_start_coord, line_chr_end_coord, line_parts))
+                    else:
+                        # Calculate individual alignment length for each line
+                        line_aligned_length = int(line_parts[6]) / length * 100 if len(line_parts) > 10 else 0
 
-                    # Calculate individual alignment length for each line
-                    line_aligned_length = int(line_parts[6]) / length * 100 if len(line_parts) > 10 else 0
+                        # Only add lines with aligned_percent <= 100
+                        if line_aligned_length <= 100:
+                            # Calculate identity, mismatches, gaps, etc.
+                            line_identity = float(line_parts[10]) if len(line_parts) > 10 else 0
+                            line_mismatch = int(line_parts[12]) if len(line_parts) > 10 else int(line_parts[2]) - int(line_parts[1])
+                            line_gap = int(line_parts[13]) if len(line_parts) > 10 else int(line_parts[2]) - int(line_parts[1])
+                            line_insertion = int(line_parts[8]) if len(line_parts) > 10 else 0
+                            line_deletion = int(line_parts[7]) if len(line_parts) > 10 else 0
+                            line_missing_fragment = 1 if len(line_parts) <= 10 else 0
 
-                    # Only add lines with aligned_percent <= 100
-                    if line_aligned_length <= 100:
-                        # Calculate identity, mismatches, gaps, etc.
-                        line_identity = float(line_parts[10]) if len(line_parts) > 10 else 0
-                        line_mismatch = int(line_parts[12]) if len(line_parts) > 10 else int(line_parts[2]) - int(line_parts[1])
-                        line_gap = int(line_parts[13]) if len(line_parts) > 10 else int(line_parts[2]) - int(line_parts[1])
-                        line_insertion = int(line_parts[8]) if len(line_parts) > 10 else 0
-                        line_deletion = int(line_parts[7]) if len(line_parts) > 10 else 0
-                        line_missing_fragment = 1 if len(line_parts) <= 10 else 0
+                            # Create a unique name for each line and append it to the result
+                            line_ref_name = ref_name.split("|")[0]
+                            result_data.append({
+                                'Group_name': f"{line_ref_name}_like{line_idx+1}|{length}",
+                                'ref_start': line_ref_start_coord,
+                                'ref_end': line_ref_end_coord,
+                                'chr_start': line_chr_start_coord,
+                                'chr_end': line_chr_end_coord,
+                                'identity_percent': f"{line_identity:.2f}",
+                                'aligned_percent': f"{line_aligned_length:.2f}",
+                                'missing_exons': line_missing_fragment,
+                                'mismatch_sum': line_mismatch,
+                                'gap_sum': line_gap,
+                                'insertions': line_insertion,
+                                'deletion': line_deletion,
+                            })
+                    previous_end_coord = int(line_parts[2])
+#                print ("fragments here")
+#                print (fragment_groups)
+                fragment_percent = sum([int(row[4][6]) for row in fragment_groups]) / length * 100
+                fragment_chr_min_start_coord = min(int(row[2]) for row in fragment_groups)
+                fragment_chr_max_end_coord = max(int(row[3]) for row in fragment_groups)
 
-                        # Create a unique name for each line and append it to the result
-                        line_ref_name = ref_name.split("|")[0]
-                        result_data.append({
-                            'Group_name': f"{line_ref_name}_like{line_idx+1}|{length}",
-                            'Subgroup': line_idx + 1,
-                            'ref_start': line_ref_start_coord,
-                            'ref_end': line_ref_end_coord,
-                            'chr_start': line_chr_start_coord,
-                            'chr_end': line_chr_end_coord,
-                            'identity_percent': f"{line_identity:.2f}",
-                            'aligned_percent': f"{line_aligned_length:.2f}",
-                            'missing_exons': line_missing_fragment,
-                            'mismatch_sum': line_mismatch,
-                            'gap_sum': line_gap,
-                            'insertions': line_insertion,
-                            'deletion': line_deletion,
-                        })
+                fragment_ref_min_start_coord = min(int(row[0]) for row in fragment_groups)
+                fragment_ref_max_end_coord = max(int(row[1]) for row in fragment_groups)
+                
+                fragment_identity = mean([float(row[4][10]) for row in fragment_groups])
+
+                fragment_missing_fragment = 1 if any(len(row[4]) <= 20 for row in fragment_groups) else 0
+                fragment_deletion = sum([int(row[4][7]) for row in fragment_groups])
+                fragment_insertion = sum([int(row[4][8]) for row in fragment_groups])
+
+                fragment_mismatch = sum([int(row[4][12]) for row in fragment_groups])
+                fragment_gap = sum([int(row[4][13]) for row in fragment_groups])
+
+                result_data.append({
+                    'Group_name': f"{line_ref_name}_like{idx+2}|{length}",
+                    'ref_start': fragment_ref_min_start_coord,
+                    'ref_end': fragment_ref_max_end_coord,
+                    'chr_start': fragment_chr_min_start_coord,
+                    'chr_end': fragment_chr_max_end_coord,
+                    'identity_percent': f"{fragment_identity:.2f}",
+                    'aligned_percent': f"{fragment_percent:.2f}",
+                    'missing_exons': fragment_missing_fragment,
+                    'mismatch_sum': fragment_mismatch,
+                    'gap_sum': fragment_gap,
+                    'insertions': fragment_insertion,
+                    'deletion': fragment_deletion,
+                })
 
             else:  # Process whole subgroup if aligned_length <= 100
                 min_ref_start_coord = min(ref_start_coord for ref_start_coord, _, _, _, _, _ in subgroup)
@@ -125,7 +166,6 @@ def group_by_ref_name(file_path):
 
                 result_data.append({
                     'Group_name': ref_name,
-                    'Subgroup': idx + 1,
                     'ref_start': min_ref_start_coord,
                     'ref_end': max_ref_end_coord,
                     'chr_start': min_chr_start_coord,
