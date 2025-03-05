89ab6e988845834c446ed5204aaf160ddfd920ff
GiangLeN <giangln@gmail.com>
2025-03-05 15:37:26 +0100

    library format

--- scripts/lib_format.py
+++ scripts/lib_format.py
@@ -20,6 +20,7 @@ args = parser.parse_args()
 
 with open(args.input_fasta, "r") as infile:
     for record in SeqIO.parse(infile, "fasta"):
+        record.seq = record.seq.replace("-", "")
         seq_length = len(record.seq)
         parts = record.id.split("|")
 
@@ -33,31 +34,3 @@ with open(args.input_fasta, "r") as infile:
         record.description = ""
 
         SeqIO.write(record, sys.stdout, "fasta")
-
-'''
-def remove_dashes(sequence):
-  seq_obj = Seq(sequence)
-  cleaned_seq = str(seq_obj).replace("-", "")
-  return cleaned_seq
-
-def process_fasta(input_fasta):
-
-    with open(input_fasta, "r") as infile:
-        for record in SeqIO.parse(infile, "fasta"):
-            parts = record.id.split("|")
-
-            # Remove dashes from the sequence
-            cleaned_sequence = remove_dashes(str(record.seq))
-            record.seq = Seq(cleaned_sequence) #update the seq object
-            seq_length = len(record.seq) #update the length after dash removal
-
-            if len(parts) > 1 and parts[-1].isdigit() and int(parts[-1]) == seq_length:
-                SeqIO.write(record, sys.stdout, "fasta")
-                continue  # Skip to the next record
-
-            new_id = f"{'|'.join(parts[:-1]) if len(parts) > 1 else record.id}|{seq_length}"
-            record.id = new_id
-            record.description = ""
-
-            SeqIO.write(record, sys.stdout, "fasta")
-'''
